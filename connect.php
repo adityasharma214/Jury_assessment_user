11dbb715cd08f5156d6d620db5f89c32b9ff7b21
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-06-13 19:37:43 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "jury.cufga4wez9ub.ap-south-1.rds.amazonaws.com";
+$username = "aditya";
+$password = "aditya123456";
 $db = "jury";
 
 // Create connection
